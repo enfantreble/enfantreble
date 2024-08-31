ea17e70c67f17d0d502e49c023775146f8b48e04
enfantreble <6405543+enfantreble@users.noreply.github.com>
2024-08-31 21:24:02 +0900

    Update graphqlInterface.js

--- res/js/graphqlInterface.js
+++ res/js/graphqlInterface.js
@@ -1,28 +1,48 @@
 // graphqlInterface.js
-(async function() {
-    const API_URL = 'http://localhost:9000/graphql';
+const API_URL = 'http://localhost:9000/graphql';
 
-    async function createPerformer(performerName) {
-        const mutation = `
-            mutation {
-                performerCreate(input: {name: "${performerName}"}) {
+async function findPerformer(performerName) {
+    const query = `
+        query {
+            findPerformers(filter: {per_page: 10, page: 1}, performer_filter: {}, q: "${performerName}") {
+                performers {
                     id
                     name
+                    disambiguation
+                    alias_list
                 }
             }
-        `;
-        const response = await fetch(API_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ query: mutation })
-        });
+        }
+    `;
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
+    });
 
-        const data = await response.json();
-        return data;
-    }
+    const data = await response.json();
+    return data.data.findPerformers.performers;
+}
 
-    // Expose the function to the global scope
-    window.createPerformer = createPerformer;
-})();
+async function createPerformer(performerDetails) {
+    const mutation = `
+        mutation {
+            performerCreate(input: {name: "${performerDetails.name}"}) {
+                id
+                name
+            }
+        }
+    `;
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query: mutation })
+    });
+
+    const data = await response.json();
+    return data.data.performerCreate;
+}
