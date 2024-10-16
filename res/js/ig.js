22e443e631f1736487d33621f6ca4f54c7f5b1fc
enfantreble <6405543+enfantreble@users.noreply.github.com>
2024-10-17 01:16:13 +0900

    Update ig.js

--- res/js/ig.js
+++ res/js/ig.js
@@ -5,11 +5,12 @@
  */
 function parseJsonInstagram(jsonData) {
     const mediaMap = new Map();
-
+    console.log('parsing');
     if (jsonData.data && jsonData.data.xdt_api__v1__feed__timeline__connection && Array.isArray(jsonData.data.xdt_api__v1__feed__timeline__connection.edges)) {
         const edges = jsonData.data.xdt_api__v1__feed__timeline__connection.edges;
         const username = edges[0]?.node?.user?.username || 'unknown_user'; // Extract username
         edges.forEach(edge => {
+            console.log('iteratin');
             const media = edge.node.media;
             const mediaId = media.id; // Unique media ID
             const timestamp = media.taken_at || Date.now(); // Get timestamp or current time
