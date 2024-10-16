97fdae54806b7098054465b70440221297b6268b
enfantreble <6405543+enfantreble@users.noreply.github.com>
2024-10-16 15:59:44 +0900

    Update aria.js

--- res/js/aria.js
+++ res/js/aria.js
@@ -8,14 +8,22 @@ function sendToAria2(downloadUrl, secret = 'ariaisabitch') {
         'params': ['token:' + secret, [downloadUrl]]
     });
 
-    fetch(aria2Url, {
+    GM_xmlhttpRequest({
         method: 'POST',
+        url: aria2Url,
         headers: {
             'Content-Type': 'application/json'
         },
-        body: jsonPayload
-    })
-    .then(response => response.json())
-    .then(data => console.log('Aria2 response:', data))
-    .catch(error => console.error('Error:', error));
+        data: jsonPayload,
+        onload: function(response) {
+            const jsonResponse = JSON.parse(response.responseText);
+            console.log('Aria2 response:', jsonResponse);
+        },
+        onerror: function(error) {
+            console.error('Error:', error);
+        }
+    });
 }
+
+// Example usage: pass the URL to the function
+// sendToAria2("http://example.com/file-to-download");
